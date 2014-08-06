93172ccf9e8289c69b7289f2e4d554d4e3fb9569
lis0x90 <lis0x90@gmail.com>
2014-08-06 14:00:39 +0400

    Obsolete

--- deploy-lib.cmd
+++
@@ -1,8 +0,0 @@
-mvn deploy:deploy-file ^
-  -DgroupId=org.opencv ^
-  -DartifactId=opencv-java ^
-  -Dversion=2.4.9 ^
-  -Dpackaging=jar ^
-  -Dfile=lib\opencv-249.jar ^
-  -DrepositoryId=kamis-repo ^
-  -Durl=ftp://spb.kamis.ru
